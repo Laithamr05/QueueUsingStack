cbb5c9117452d1f98a778abceeedb82a7e7f64f4
Laith Amro <amrlaith05@gmail.com>
2025-04-17 10:37:03 +0300

    Add files via upload

--- StackQueue.java
+++ StackQueue.java
@@ -1,58 +1,59 @@
 import java.util.Stack;
 
-public class StackQueue<T> implements QueueInterface<T>{
-	Stack<T> stack1;
+public class StackQueue<T> implements QueueInterface<T> {
+    Stack<T> stack1;
     Stack<T> stack2;
 
     public StackQueue() {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
-@Override
-public void enqueue(T data) {
-stack1.push(data);
-}
 
-@Override
-public T dequeue() {
-	// TODO Auto-generated method stub
-	if (isEmpty()) {
-        System.out.println("queue is empty");
+    @Override
+    public void enqueue(T data) {
+        stack1.push(data);
     }
 
-    if (stack2.isEmpty()) {
-        while (!stack1.isEmpty()) {
-            stack2.push(stack1.pop());
+    @Override
+    public T dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
         }
-    }
 
-    return stack2.pop();
-}
-@Override
-public T getFront() {
-	if (isEmpty()) {
-        System.out.println("queue is empty");
-    }
-
-    if (stack2.isEmpty()) {
-        while (!stack1.isEmpty()) {
-            stack2.push(stack1.pop());
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
         }
+
+        return stack2.pop();
     }
 
-    return stack2.peek();
-}
+    @Override
+    public T getFront() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
+        }
 
-@Override
-public boolean isEmpty() {
-	// TODO Auto-generated method stub
-    return stack1.isEmpty() && stack2.isEmpty();
-}
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
 
-@Override
-public void clear() {
-stack1.clear();
-stack2.clear();
-}
+        return stack2.peek();
+    }
 
+    @Override
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        stack1.clear();
+        stack2.clear();
+    }
 }
